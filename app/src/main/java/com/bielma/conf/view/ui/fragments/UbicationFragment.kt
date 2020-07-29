5e96b6384d68c1a5c7c4b9f527c9beb446a941c0
Antonio <thetonybl@gmailc.om>
2020-07-29 14:59:35 -0500

    Marcador en el mapa

--- app/src/main/java/com/bielma/conf/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/bielma/conf/view/ui/fragments/UbicationFragment.kt
@@ -1,14 +1,24 @@
 package com.bielma.conf.view.ui.fragments
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.bielma.conf.R
+import com.bielma.conf.model.Location
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
 class UbicationFragment : Fragment(), OnMapReadyCallback {
@@ -34,7 +44,26 @@ class UbicationFragment : Fragment(), OnMapReadyCallback {
 
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val location = Location()
+        val zoom = 16f
+        val centerMap = LatLng(location.latitude, location.longitude)
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap, zoom))
+        val markerOptions = MarkerOptions()
+        markerOptions.position(centerMap)
+        markerOptions.title( "Platzi Conf 2019")
+        val bitmapDraw = context?.applicationContext?.let {
+            ContextCompat.getDrawable(it, R.drawable.logo_platzi) as BitmapDrawable
+        }
+
+
+        val smallMarker = Bitmap.createScaledBitmap(bitmapDraw!!.bitmap, 150, 150, false)
+
+
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+        googleMap?.addMarker(markerOptions)
+
+
 
     }
 }
